5c42488a53501e00d058963c15b3e661a4a7cf31
Matthew Chan <matt@themattchan.com>
2014-11-08 03:13:45 -0800

    fixed compiler errors, refactored code

---
+++ src/bop.h
@@ -0,0 +1,9 @@
+typedef enum {start, pick_action, check, update} STATE;
+typedef enum {bop, pull, twist} ACTION;
+
+void state(void);
+void handle_pick_action(void);
+void handle_check(void);
+void handle_init(void);
+void handle_deinit(void);
+static void timer_callback(void *data);
