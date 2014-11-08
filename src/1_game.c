b42f476750a5eee7831e5baa9787854726f15a8c
Matthew Chan <matt@themattchan.com>
2014-11-08 15:57:49 -0800

    cleanup & comments

--- src/1_game.c
+++ src/1_game.c
@@ -5,27 +5,13 @@ static Window *window;
 static TextLayer *text_layer;
 static AppTimer *timer;
 
-static int randInt = -1;
-
 int count = 0;
 double time_interval = 5000;
 
 STATE mState = start;
 
-ACTION mAction = none;
-ACTION mGesture = none;
-
-static void select_click_handler(ClickRecognizerRef recognizer, void *context) {
-	if (mState == start) {
-		mState = pick_action;
-		state();
-	} else if (mState == end) {
-		mState = start;
-		count = 0;
-		time_interval = 5000;
-		text_layer_set_text(text_layer, "START");
-	}
-}
+ACTION mAction = none;			/* Action we want */
+ACTION mGesture = none;			/* Action that user inputs */
 
 /* Need implementations for unused handlers */
 static void up_click_handler(ClickRecognizerRef recognizer, void *context) {}
@@ -37,6 +23,7 @@ static void click_config_provider(void *context) {
 	window_single_click_subscribe(BUTTON_ID_DOWN, down_click_handler);
 }
 
+/* Setup UI -- Window handlers for load and unload */
 static void window_load(Window *window) {
 	Layer *window_layer = window_get_root_layer(window);
 	GRect bounds = layer_get_bounds(window_layer);
@@ -45,15 +32,15 @@ static void window_load(Window *window) {
 			.origin = { 0, 60 },
 			.size = { bounds.size.w, 40 }
 		});
-	//set text attributes
+	// set text attributes
 	text_layer_set_text(text_layer, "START");
 	text_layer_set_text_color(text_layer, GColorBlack);
 	text_layer_set_text_alignment(text_layer, GTextAlignmentCenter);
 	text_layer_set_font(text_layer, fonts_get_system_font(FONT_KEY_GOTHIC_28_BOLD));
-	//set background color
+	// set background color
 	text_layer_set_background_color(text_layer, GColorClear);
 	layer_add_child(window_layer, text_layer_get_layer(text_layer));
-	//turn backlight on
+	// turn backlight on
 	light_enable(true);
 }
 
@@ -61,13 +48,14 @@ static void window_unload(Window *window) {
 	text_layer_destroy(text_layer);
 }
 
+/* Our logic for setup and teardown -- call in the main driver */
 void bop_init(void) {
 	window = window_create();
 	window_set_click_config_provider(window, click_config_provider);
 	window_set_window_handlers(window, (WindowHandlers) {
 			.load = window_load,
-				.unload = window_unload
-				});
+			.unload = window_unload
+	});
 	const bool animated = true;
 	window_stack_push(window, animated);
 
@@ -79,6 +67,25 @@ void bop_deinit(void) {
 	accel_tap_service_unsubscribe();
 }
 
+/* GAME LOGIC */
+/* START WITH MIDDLE BUTTON PUSH */
+static void select_click_handler(ClickRecognizerRef recognizer, void *context) {
+	if (mState == start) {
+		mState = pick_action;
+		state();
+	} else if (mState == end) {
+		mState = start;
+		mAction = none;
+		mGesture = none;
+
+		count = 0;
+		time_interval = 5000;
+
+		text_layer_set_text(text_layer, "START");
+	}
+}
+
+/* State machine control flow */
 void state(void) {
 	switch (mState) {
 	case pick_action:
@@ -93,9 +100,8 @@ void state(void) {
 }
 
 void accel_tap_handler(AccelAxisType axis, int32_t direction) {
-	if(mState==start)
-		return;
-	switch (axis) {
+	if (mState == pick_action) {
+		switch (axis) {
 		case ACCEL_AXIS_X:
 			mGesture = pull;
 			text_layer_set_text(text_layer, "pull!");
@@ -110,21 +116,15 @@ void accel_tap_handler(AccelAxisType axis, int32_t direction) {
 			break;
 		default:
 			break;
+		}
 	}
 }
 
-void handle_end(void) {
-	/*char score[sizeof(int)];
-	snprintf(score, sizeof(int), "%d", count);*/
-	text_layer_set_text(text_layer, "END");
-	
-	vibes_double_pulse();
-}
-
 void handle_pick_action(void) {
+	static int randInt = -1;
 	if (randInt == -1)
 		srand(time(NULL));
-	randInt = rand()%3;
+	randInt = rand() % 3;
 	switch (randInt) {
 	case bop:
 		mAction = bop;
@@ -141,9 +141,9 @@ void handle_pick_action(void) {
 	default:
 		return;
 	}
-	//haptic feedback for new action
+	// haptic feedback for new action
 	vibes_short_pulse();
-	//(re)start Timer
+	// (re)start Timer
 	timer = app_timer_register(time_interval, timer_callback, NULL);
 }
 
@@ -161,4 +161,12 @@ void handle_check(void) {
 static void timer_callback(void *data) {
 	mState = check;
 	state();
+}
+
+void handle_end(void) {
+	/* char score[sizeof(int)]; */
+	/* snprintf(score, sizeof(int), "%d", count); */
+	text_layer_set_text(text_layer, "END");
+
+	vibes_double_pulse();
 }
