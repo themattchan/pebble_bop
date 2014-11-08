3a613ea05322e3512d491ec4acc0fcfed4808401
Jesus Garcia <jesusega@usc.edu>
2014-11-08 12:27:42 -0800

    Created gui header file that contains user-defined functions for image creation, deletion, and display

---
+++ src/gui.h
@@ -0,0 +1,48 @@
+/* Function: createLayer
+ * Creates a layer with the bounds of the screen to display an image 
+ * Param: window - the window of the app
+ * Returns: a bitmap layer for displaying images
+ */
+BitmapLayer* createLayer(Window* window) {
+	Layer *window = window_get_root_layer(window);
+	GRect bounds = layer_get_bounds(window);
+	BitmapLayer* layer = bitmap_layer_create(bounds);
+	bitmap_layer_set_alignment(layer, "GAlignCenter");
+	return layer;
+}
+
+/* Function: createImage
+ * Creates a bitmap using the resource id in the .json file
+ * Param: id - the resource id of the image
+ * Returns: a bitmap image
+ */
+GBitmap* createImage(uint32_t id) {
+	return gbitmap_create_with_resource(id);
+}
+
+/* Function: displayImage
+ * Adds the image onto the bitmap layer to display it
+ * Param: layer - the bitmap layer, image - the bitmap being displayed
+ * Returns: nothing
+ */
+void displayImage(BitmapLayer* layer, GBitmap* image) {
+	bitmap_layer_set_bitmap(layer, image);
+}
+
+/* Function: deleteImage
+ * Unloads the image displayed
+ * Param: image - the image being removed
+ * Returns: nothing
+ */
+void deleteImage(GBitmap* image) {
+	gbitmap_destroy(image);
+}
+
+/* Function: deleteLayer
+ * Deletes a bitmap layer
+ * Param: layer - the bitmap layer being deleted
+ * Returns: nothing
+ */
+void deleteLayer(BitmapLayer* layer) {
+	bitmap_layer_destroy(layer);
+}
