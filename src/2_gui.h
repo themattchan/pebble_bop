bdc4a42d91cbe045ff710eed4774860146398454
Jesus Garcia <jesusega@usc.edu>
2014-11-09 00:00:56 -0800

    Fixed bugs 9

--- src/2_gui.h
+++ src/2_gui.h
@@ -8,6 +8,7 @@
 BitmapLayer* createLayer(GRect bounds) {
 	BitmapLayer* layer = bitmap_layer_create(bounds);
 	bitmap_layer_set_alignment(layer, GAlignCenter);
+	bitmap_layer_set_background_color(layer, GColorClear);
 	return layer;
 }
 
