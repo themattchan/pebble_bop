d6c18f5cde66f36e0e29baeea147b823c19ce21f
Jesus Garcia <jesusega@usc.edu>
2014-11-09 09:26:59 -0800

    Title

--- src/game.c
+++ src/game.c
@@ -86,6 +86,7 @@ static void window_load(Window *window) {
 	// set background color
 	text_layer_set_background_color(text_layer, GColorClear);
 	layer_add_child(window_layer, text_layer_get_layer(text_layer));
+	layer_add_child(window_layer, bitmap_layer_get_layer(bitmap_layer));
 	// turn backlight on
 	light_enable(true);
 }
